9deee05b8aef736d7d008f16a74aa357331c5780
tqhoa <tqhoa8th@gmail.com>
2021-03-07 21:21:48 +0700

    additional taiwincss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,3 @@
 <template>
   <h1>Home Page</h1>
-  <button @click="inc">{{ count }}</button>
 </template>
-
-<script lang="ts">
-  import { ref } from 'vue'
-
-
-  export default {
-    setup() {
-      const count = ref(0)
-      const inc = () => count.value++
-
-      return {
-        count,
-        inc,
-      }
-    },
-  }
-</script>
